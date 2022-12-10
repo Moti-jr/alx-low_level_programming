9aed6574ebc9ebb9115caf0cb8a17d28e1c20f3b
moti-jnr <kipchimoti@gmail.com>
2022-12-10 09:17:40 -0800

    final one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
-/*sheldon
- * 
+/*sheldon kipchirchir
  */
-
-#include <stdio.h>
-
 /**
  * main - Prints "Programming is like building a multilingual
  *                puzzle, followed by a new line.
