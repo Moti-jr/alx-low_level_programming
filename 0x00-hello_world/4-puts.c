e8343c3e7ff7188ca4040a12aad9f0d51aef0a95
moti-jnr <kipchimoti@gmail.com>
2022-12-10 06:15:35 -0800

    answer

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 /*sheldon
+ * 
+ * */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-char a[]="saravan";
-char b[]="hajsof";
-puts(a);
-puts(b);
-return 0;
+puts("Programming is like building a multilingual puzzle");
+
+return (0);
+}
+
+
+
+
+
+
+
 }
