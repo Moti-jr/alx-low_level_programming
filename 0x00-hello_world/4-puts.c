3b0d76e30235b046a09bddb5ff5e70d704d39451
moti-jnr <kipchimoti@gmail.com>
2022-12-10 08:24:55 -0800

    final

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,41 +7,15 @@
 
 #include <stdio.h>
 
->>>>>>> e8343c3e7ff7188ca4040a12aad9f0d51aef0a95
 /**
  * main - Prints "Programming is like building a multilingual
  *                puzzle, followed by a new line.
  *
-<<<<<<< HEAD
- *  * main - Entry point
- *
- *   *
- *
- * * Return: Always 0 (Success)
- *
- */
-
-=======
  * Return: Always 0.
  */
->>>>>>> e8343c3e7ff7188ca4040a12aad9f0d51aef0a95
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
-}
-
-
-
-
-
-
-<<<<<<< HEAD
-		puts("\"Programming is like building a multilingual puzzle");
-
-		return (0);
-=======
->>>>>>> e8343c3e7ff7188ca4040a12aad9f0d51aef0a95
-
+	return (0);
 }
