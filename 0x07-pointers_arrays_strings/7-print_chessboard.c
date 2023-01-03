7e2e7731f8e61a3362868ef4af28317e208564fc
moti-jnr <kipchimoti@gmail.com>
2023-01-03 05:12:10 -0800

    some errors

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * print_chessboard - a function that prints the chessboard.
+ * print_chessboard - prints the chessboard
  * @a: pointer to pieces to print
  *
  * Return: void
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
