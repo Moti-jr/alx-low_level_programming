173dc02b449ab33e854ecf84e7fceda34e096380
moti-jnr <kipchimoti@gmail.com>
2023-01-03 05:03:18 -0800

    solving some errors

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,26 @@
 #include "main.h"
 
 /**
- * _memset - fills memory block with a constant byte
- * @s: memory block address
- * @b: char to be used
- * @n: int parameter
- * Return: pointer to the memory block
+ * _memset - function fill the first @n bytes of the memory area pointed
+ * to by @s with the constant byte @b
+ *
+ * @n: bytes of the memory area pointed to by @s
+ *
+ * @s: with the constant byte @b
+ *
+ * @b: memory area pointer
+ *
+ * Return: a pointer to the memory area @s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n)
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		s[n - 1] = b;
-		n--;
+	s[i] = b;
+	i++;
 	}
+
 	return (s);
 }
